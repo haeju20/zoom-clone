6f593dcd9ce089fd4e281d052c5a9fbba19f492d
jisu <1712wltn@gmail.com>
2022-11-20 14:03:40 +0900

    1120 websocket

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import http from "http";
+import SocketIO from "socket.io";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const httpServer = http.createServer(app); //http server create
+const wsServer = SocketIO(httpServer);
+
+
+
+/*
+const sockets = [];
+const wss = new WebSocket.Server({server}); //websocket server create
+function handleConnection(socket) {
+    console.log(socket);
+};
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
+    console.log("Connected to Server✅");
+    socket.on("close", () => {
+        console.log("Disconnected from the Browser❌");
+    });
+ 
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => 
+                aSocket.send(`${socket.nickname}: ${message.payload}`));    
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }  
+    });
+});
+*/
+
+httpServer.listen(3000, handleListen);
